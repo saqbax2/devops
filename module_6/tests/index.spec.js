b83694c9464e8227732552eba4194972c47cc9e1
saqbax2 <86385606+saqbax2@users.noreply.github.com>
2021-07-27 18:25:32 -0500

    Update index.spec.js

--- module_6/tests/index.spec.js
+++ module_6/tests/index.spec.js
@@ -1,8 +1,8 @@
 const plus = require("./index")
-test("Returns 4", () => {
+test(" 4", () => {
     expect(calculator.plus(2,2)).toBe(4);
 });
 
 test("Returns 0", () => {
-    expect(calculator.minus(2,2)).toBe(0);
+    expect(calculator.resta(2,2)).toBe(0);
 });
